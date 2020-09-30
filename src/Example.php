5b944ab25b3189e6d36bfd80c07cd803b2e6d3f5
protoku <hello@protoku.be>
2020-09-30 22:55:16 +0200

    wip

---
+++ src/Example.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace :Vendor\:Package_name;
+
+final class Example
+{
+    public function foo(): string
+    {
+        return 'bar';
+    }
+}
---
+++ tests/ExampleTest.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace :Vendor\:Package_name;
+
+class ExampleTest extends \PHPUnit\Framework\TestCase
+{
+    public function testTrueIsTrue()
+    {
+        $this->assertTrue(true);
+    }
+}
